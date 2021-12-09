2168b3850fa680b5ade7c2741d2ab84dcd135166
zhaoyongqiang <zhaoyongqiang@agora.io>
2021-12-09 14:17:28 +0800

    Initial Commit

---
+++ AgoraKaraokeScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgoraKaraokeScore
+//
+//  Created by zhaoyongqiang on 2021/12/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
